2952108a2eb4f64f28befa669ee60d86846a3bc4
Palak-web <palak55544@gmail.com>
2026-02-08 23:26:31 +0530

    Add basic number checking exercises (even/odd and positive/negative)

---
+++ Exercises/positive_negative_num.py
@@ -0,0 +1,7 @@
+num = int(input("Enter your number: "))
+if num > 0:
+    print(f"{num} is a positive number")
+elif num == 0:
+    print("Zero is a natural number")
+else:
+    print(f"{num} is a negative number")
