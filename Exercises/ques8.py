a0790b245c6971913113b1e61f3a07567ec01339
Palak-web <palak55544@gmail.com>
2026-03-08 18:17:38 +0530

    Add number_to_string function

---
+++ Exercises/ques8.py
@@ -0,0 +1,8 @@
+# We need a function that can transform a number (integer) into a string.
+def number_to_string(num):
+    return str(num)
+
+
+result = number_to_string(123)
+print(result)
+print(type(result))
